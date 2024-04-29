55c9a92f4d4554a2024207986c987ee6b690b5a9
Jakub Przybylski <jakubp02.10@gmail.com>
2024-04-29 21:08:55 +0200

    Task4 done.

--- src/zad4/Author.java
+++ src/zad4/Author.java
@@ -1,11 +1,39 @@
-///**
-// *
-// *  @author Przybylski Jakub S24512
-// *
-// */
-//
-//package zad4;
-//
-//
-//public class Author implements Runnable {
-//}
+/**
+ *
+ *  @author Przybylski Jakub S24512
+ *
+ */
+
+package zad4;
+
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Author implements Runnable {
+    private final String[] msgs;
+    private final BlockingQueue<String> queue;
+    public Author(String[] xyz) {
+        this.msgs = xyz;
+        queue = new ArrayBlockingQueue<>(msgs.length);
+    }
+    @Override
+    public void run() {
+        for(String msg : msgs){
+            queue.add(msg);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public String getMsgFromQueue(){
+        try {
+            return queue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/zad4/Writer.java
+++ src/zad4/Writer.java
@@ -1,11 +1,23 @@
-///**
-// *
-// *  @author Przybylski Jakub S24512
-// *
-// */
-//
-//package zad4;
-//
-//
-//public class Writer implements Runnable {
-//}
+/**
+ *
+ *  @author Przybylski Jakub S24512
+ *
+ */
+
+package zad4;
+
+
+public class Writer implements Runnable {
+    Author author;
+    public Writer(Author autor) {
+        this.author = autor;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            String msg = author.getMsgFromQueue();
+            System.out.println("Msg: " + msg);
+        }
+    }
+}
